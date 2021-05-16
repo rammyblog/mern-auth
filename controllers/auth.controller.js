4fd1a2daadf247667e599bc2c8ee37a79b37c210
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-05-17 00:13:31 +0100

    Added sendgrid

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const ErrorResponse = require("../utils/ErrorResponse");
-
+const sendEmail = require("../utils/sendEmail");
 exports.register = async (req, res, next) => {
   const { username, email, password } = req.body;
   try {
@@ -41,18 +41,31 @@ exports.forgotPassword = async (req, res, next) => {
     const resetToken = user.getResetPasswordToken();
     await user.save();
 
-    const resetURL = `${process.env.FRONTEND_URL}/password-reset/${resetToken}`
+    const resetURL = `${process.env.FRONTEND_URL}/password-reset/${resetToken}`;
     const message = `
       <h1>You have requested a password reset</h1>
       <p>Please go to this link to reset your password </p>
-      <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
-    `
+      <a href=${resetURL} clicktracking=off>${resetURL}</a>
+    `;
     try {
-      
+      await sendEmail({
+        to: user.email,
+        subject: "Password reset request",
+        html: message,
+        from: process.env.EMAIL_FROM,
+      });
+      res.status(200).json({ success: true, data: "Email Sent" });
     } catch (error) {
-      
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
+      await user.save();
+      console.log(error);
+      return next(new ErrorResponse("Email could not be sent", 500));
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 exports.resetPassword = (req, res, next) => {
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,27 +1,40 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const sendEmail = (options) => {
-  const transporter = nodemailer.createTransporter({
-    service: process.env.EMAIL_SERVICE,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: options.to,
-    subject: options.subject,
-    html: options.text,
-  };
+// const sendEmail = (options) => {
+//   const transporter = nodemailer.createTransport({
+//     service: process.env.EMAIL_SERVICE,
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
+//   const mailOptions = {
+//     from: process.env.EMAIL_FROM,
+//     to: options.to,
+//     subject: options.subject,
+//     html: options.text,
+//   };
 
-  transporter.sendMail(mailOptions, function (err, info) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(info);
-    }
-  });
+//   transporter.sendMail(mailOptions, function (err, info) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(info);
+//     }
+//   });
+// };
+
+const sgMail = require("@sendgrid/mail");
+
+const sendEmail = async (emailOptions) => {
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  try {
+    await sgMail.send(emailOptions);
+    console.log("Email sent");
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = sendEmail;
